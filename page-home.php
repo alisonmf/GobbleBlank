1db03026df7365029c390f9438234da7e56ab6e7
Alison Foxall <alison.mf@gmail.com>
2011-08-04 11:31:30 -0400

    removed blog entry stuff
    generally I don't use this on home pages unless the site caters to
    time-sensitive data. removed class entry for home page.

--- page-home.php
+++ page-home.php
@@ -8,15 +8,7 @@
 
 			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 
-			<div class="entry">
 				<?php the_content(); ?>
-			</div>
-
-			<div class="postmetadata">
-				<?php the_tags('Tags: ', ', ', '<br />'); ?>
-				Posted in <?php the_category(', ') ?> | 
-				<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
-			</div>
 
 		</div>
 
