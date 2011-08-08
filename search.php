4f6d4ba3eeefbb73aad27d35bf1adc7426952f06
Alison Foxall <alison.mf@gmail.com>
2011-08-08 15:51:56 -0400

    added permalink to h2's, removed meta information

--- search.php
+++ search.php
@@ -10,9 +10,7 @@
 
 			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 
-				<h2><?php the_title(); ?></h2>
-
-				<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
 				<div class="entry">
 
