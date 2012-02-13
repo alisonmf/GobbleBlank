11e32f83c250170fed532b239754d607bdbb9a8f
Alison Foxall <alison.mf@gmail.com>
2012-02-13 13:21:23 -0500

    rearranged the wp_head underneath the JS comment block

--- header.php
+++ header.php
@@ -35,10 +35,9 @@
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	
 		<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-		
-		<?php wp_head(); ?>
-		
+			
 		<!-- JS -->
+		<?php wp_head(); ?>
 		
 	</head>
 
