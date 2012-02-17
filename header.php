f0f27233ba47f3eb83bcc6196a5bd9128057e9c7
Alison Foxall <alison.mf@gmail.com>
2012-02-17 11:21:11 -0500

    added conditionals for ie9
    just in case

--- header.php
+++ header.php
@@ -8,7 +8,10 @@
 <!--[if IE 8]>
 <html id="ie8" <?php language_attributes(); ?>>
 <![endif]-->
-<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<!--[if IE 9]>
+<html id="ie9" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8) | !(IE 9)  ]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 	<head>
