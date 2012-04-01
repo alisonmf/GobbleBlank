68a7fe56e71a5dd4f86179a37c7dd4d0d60df9a1
Alison Foxall <alison.mf@gmail.com>
2012-04-01 02:43:13 -0400

    can't use class page here when body uses on a page

--- page.php
+++ page.php
@@ -4,7 +4,7 @@
 
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				
-			<div class="page" id="post-<?php the_ID(); ?>">
+			<div id="post-<?php the_ID(); ?>">
 	
 				<h2><?php the_title(); ?></h2>
 	
