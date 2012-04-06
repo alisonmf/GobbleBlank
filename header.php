75828a26d7d4f276780fd142edac5a1672f94736
Alison Foxall <alison.mf@gmail.com>
2012-04-06 10:20:21 -0400

    oops

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <!doctype html>
 
-<!--[if lt IE 7 ]> <html id="ie6" class="ie" <?php language_attributes(); ?> <![endif]-->
-<!--[if IE 7 ]>    <html id="ie7" class="ie" <?php language_attributes(); ?> <![endif]-->
-<!--[if IE 8 ]>    <html id="ie8" class="ie" <?php language_attributes(); ?> <![endif]-->
-<!--[if IE 9 ]>    <html id="ie9" class="ie" <?php language_attributes(); ?> <![endif]-->
+<!--[if lt IE 7 ]> <html id="ie6" class="ie" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7 ]>    <html id="ie7" class="ie" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8 ]>    <html id="ie8" class="ie" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 9 ]>    <html id="ie9" class="ie" <?php language_attributes(); ?>> <![endif]-->
 
 <!--[if !(IE 6) | !(IE 7) | !(IE 8) | !(IE 9)  ]><!-->
 <html <?php language_attributes(); ?>>
