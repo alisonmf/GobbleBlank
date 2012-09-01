32d0f917aa17b09711cbe747c6ae57585e9e7520
Alison Foxall <alison.mf@gmail.com>
2012-09-01 15:01:09 -0400

    changed to meaningful classes

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
-<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
+<form action="<?php bloginfo('siteurl'); ?>" id="searchForm" method="get">
     <div>
-        <input type="text" id="s" name="s" value="" />
+        <input type="text" id="searchInput" name="searchInput" value="" />
         
-        <input type="submit" value="Search" id="searchsubmit" />
+        <input type="submit" value="Search" id="searchSubmit" />
     </div>
 </form>
