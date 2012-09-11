c48576b4216ac1c62a51eafe04352ffa85efa783
Alison Foxall <alison.mf@gmail.com>
2012-09-10 23:35:10 -0400

    well, now i know why it was called 's'

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form action="<?php bloginfo('siteurl'); ?>" id="searchForm" method="get">
     <div>
-        <input type="text" id="searchInput" name="searchInput" value="" />
+        <input type="text" id="s" name="s" value="" />
         
         <input type="submit" value="Search" id="searchSubmit" />
     </div>
