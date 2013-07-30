e5b61f74932a74ad550fb90e9b6fefcd3b22167e
Alison Foxall <afox@gobblelogic.com>
2013-07-30 16:25:34 -0400

    Modifications to the 404

--- 404.php
+++ 404.php
@@ -2,9 +2,9 @@
 
 	<div class="content">
 
-		<h2>Gobble Gobble Gobble. We don't know what happened. We just Gobble. Gobble Gobble Gobble.</h2>
+		<h2>Oops! What happened?</h2>
 		
-		<p>Return <a href="<?php echo home_url(); ?>">Home</a></p>
+		<p>We can't find the page you are looking for. Return <a href="<?php echo home_url(); ?>">Home</a>?</p>
 	
 	</div>
 
