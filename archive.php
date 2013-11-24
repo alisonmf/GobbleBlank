fd26ada9ae69e538ff0d1114988888ccd0fb7f05
Alison Foxall <afox@gobblelogic.com>
2013-11-24 14:30:36 -0500

    archive details
    added h1's instead of h2's for the title of those pages. also trimmed the_content to the_excerpt. added a go back to home page link for the nothing found error. encapsulated the sidebar in an actual bourbon span

--- archive.php
+++ archive.php
@@ -7,25 +7,25 @@
  			<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
 
 			<?php /* If this is a category archive */ if (is_category()) { ?>
-				<h2>Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category</h2>
+				<h1><?php single_cat_title(); ?></h1>
 
 			<?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-				<h2>Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h2>
+				<h1><?php single_tag_title(); ?></h1>
 
 			<?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-				<h2>Archive for <?php the_time('F jS, Y'); ?></h2>
+				<h1>Archive for <?php the_time('F jS, Y'); ?></h1>
 
 			<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-				<h2>Archive for <?php the_time('F, Y'); ?></h2>
+				<h1>Archive for <?php the_time('F, Y'); ?></h1>
 
 			<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-				<h2 class="pagetitle">Archive for <?php the_time('Y'); ?></h2>
+				<h1>Archive for <?php the_time('Y'); ?></h1>
 
 			<?php /* If this is an author archive */ } elseif (is_author()) { ?>
-				<h2 class="pagetitle">Author Archive</h2>
+				<h1>Author Archive</h1>
 
 			<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-				<h2 class="pagetitle">Blog Archives</h2>
+				<h1>Blog Archives</h1>
 			
 			<?php } ?>
 
@@ -40,7 +40,7 @@
 						<?php get_template_part('meta'); ?>
 
 						<div class="entry">
-							<?php the_content(); ?>
+							<?php the_excerpt(); ?>
 						</div>
 
 				</div>
@@ -51,12 +51,18 @@
 			
 	<?php else : ?>
 
-		<h2>Nothing found</h2>
+		<h1>We don't have anything to show you!</h1>
+		
+		<p>Try going back to the <a href="<?php bloginfo('url'); ?>">home page</a>.</p>
 
 	<?php endif; ?>
 
-<?php get_sidebar(); ?>
-
+	</div>
+	
+	<div class="sidebar span-4">
+	
+		<?php get_sidebar(); ?>
+	
 	</div>
 
 <?php get_footer(); ?>
