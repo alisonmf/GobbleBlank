626fc2d1e393f443bfc757476428e56dd41884ff
Alison Foxall <afox@gobblelogic.com>
2013-11-24 14:35:43 -0500

    removed entry class, wrapped sidebar in span

--- page.php
+++ page.php
@@ -4,24 +4,22 @@
 
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				
-			<div class="post-<?php the_ID(); ?>">
+		<div class="post-<?php the_ID(); ?>">
+
+			<h1><?php the_title(); ?></h1>
+
+			<?php the_content(); ?>
 	
-				<h1><?php the_title(); ?></h1>
-	
-				<div class="entry">
-	
-					<?php the_content(); ?>
-	
-					<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
-	
-				</div><!-- /.entry -->
-	
-			</div><!-- /.post -->
+		</div><!-- /.post -->
 	
 		<?php endwhile; endif; ?>
 		
 	</div><!-- /.content -->	
 
-<?php get_sidebar(); ?>
+	<div class="sidebar span-4">
+	
+		<?php get_sidebar(); ?>
+	
+	</div>
 
 <?php get_footer(); ?>
