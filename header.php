9fbe705c51a7bb1ea978caaa458a979f6e284fe0
Alison Foxall <afox@gobblelogic.com>
2013-11-24 14:33:48 -0500

    header comments and compatibility
    removed the name of the blog in title. this conflicts with yeast's SEO plugin. added comments about wp_head and jquery, etc. changed path to print css, since it moved.

--- header.php
+++ header.php
@@ -17,9 +17,6 @@
 		<?php		
 		wp_title( '|', true, 'right' );
 	
-		// Add the blog name.
-		bloginfo( 'name' );
-	
 		// Add the blog description for the home/front page.
 		$site_description = get_bloginfo( 'description', 'display' );
 		if ( $site_description && ( is_home() || is_front_page() ) )
@@ -30,14 +27,28 @@
 	<!-- CSS/LINKS -->
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />	
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/print.css" type="text/css" media="print" />	
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/scss/print.css" type="text/css" media="print" />	
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 
-	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); wp_enqueue_script( 'jquery' ); ?>
+	<?php 
+	
+		//emqueue the comment reply string for single posts
+		if (is_singular()) {
+			wp_enqueue_script( 'comment-reply' );
+		}
+		
+		//enqueue jquery
+		wp_enqueue_script( 'jquery' );
+	?>
 		
 	<!-- JS -->
-	<?php wp_head(); ?>
+	<?php 
+		
+		//outputs all the plugins included front end scripts and styles
+		wp_head();
+	
+	?>
 	
 </head>
 
@@ -47,10 +58,10 @@
 
 		<div class="header">
 		
-			<a class="logo" href="<?php echo home_url(); ?>/"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/logo.png" /></a>						
+			<a class="logo" href="<?php echo home_url(); ?>/"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/logo.png" /></a>		
 			
-			<ul>
+		    <ul class="nav navbar-nav">
 				<?php wp_nav_menu( array( 'theme_location' => 'menu-main', 'items_wrap' => '%3$s' ) ); ?>
-			</ul>
+		    </ul>
 			
 		</div><!-- /#header -->
