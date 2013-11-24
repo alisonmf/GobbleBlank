9fa4b83b6c9c439f98a46474e9a9893f0e432998
Alison Foxall <afox@gobblelogic.com>
2013-11-24 14:36:49 -0500

    wrapped span classes and comments

--- single.php
+++ single.php
@@ -1,29 +1,36 @@
 <?php get_header(); ?>
 
-	<div class="content">
+	<div class="content span-8">
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	
 		<div <?php post_class() ?> class="post-<?php the_ID(); ?>">
 			
 			<h1><?php the_title(); ?></h1>
 			
-			<?php get_template_part('meta'); ?>
-
+			<?php 
+				//gets the time, category, and tags
+				get_template_part('meta'); 
+			?>
+	
 			<div class="entry">
 				
 				<?php the_content(); ?>
-
+	
 			</div>
 			
 		</div>
-
-	<?php comments_template(); ?>
-
-	<?php endwhile; endif; ?>
 	
-	<?php get_sidebar(); ?>
-
+		<?php comments_template(); ?>
+	
+		<?php endwhile; endif; ?>
+	
+	</div>
+	
+	<div class="sidebar span-4">
+	
+		<?php get_sidebar(); ?>
+	
 	</div>
 
 <?php get_footer(); ?>
