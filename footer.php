c4d08a1296adee2a8075c6a8d0a432cc83ac2265
Alison Foxall <afox@gobblelogic.com>
2013-11-24 14:31:04 -0500

    removed analytics added comment

--- footer.php
+++ footer.php
@@ -10,23 +10,12 @@
 
 	</div><!-- /#wrapper -->
 
-	<?php wp_footer(); ?>
+	<?php
 	
-	<!-- Analytics, just replace the UA number -->
+		//needed for plugins to hook into
+		wp_footer();
 	
-	<script type="text/javascript">
-	/*
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-00000000-00']);
-	  _gaq.push(['_trackPageview']);
-	
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	*/
-	</script>
+	?>
 	
 </body>
 
